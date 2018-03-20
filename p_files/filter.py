2f14929ad9bd8b07b1d5e0ed30929a548b8d9673
micheleconni <35294422+micheleconni@users.noreply.github.com>
2018-03-20 15:50:39 +0100

    Updated filter.py
    Complete filter.py uploading

--- p_files/filter.py
+++ p_files/filter.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import time
 import matplotlib.pyplot as plt
 
 
@@ -16,19 +18,67 @@ def f_gauss(lmbd, n_gauss):
     sigma = fwhm / np.sqrt(2 * np.log(2))
     # create list to store data
     gauss_list = []
-    for i in range(n_gauss):
+    for m in range(n_gauss):
         # define mean of distribution
-        mu = lmbd[0] + ((2*i+1)*fwhm)/2
+        mu = lmbd[0] + ((2*m+1)*fwhm)/2
         # save distribution in list
         gauss_list.append(gaussian(lmbd, mu, sigma))
     return gauss_list
 
 
-n = 15
+# define range of wavelengths under analysis
 waves = np.arange(400, 785, 5)
-out = f_gauss(waves, n)
-# plot the functions
-for j in out:
-    plt.plot(j)
-plt.ylabel('gaussians')
-plt.show()
+
+# get time
+time_start = time.time()
+
+# cycle on images
+for n_img in range(1, 61):
+    # put a 0 in front of the number if it has no decimals
+    if n_img < 10:
+        n_img = '0{}'.format(n_img)
+
+    # import multi-spectral files in list of arrays
+    for k in range(1, len(waves) + 1):
+        if k == 1:
+            data = np.loadtxt('../SpecTex/T{}/{}.txt'.format(n_img, k))
+            # add third dimension in order to then append other spectral data
+            data = np.expand_dims(data, axis=2)
+        else:
+            # dummy variable used to import the data and expand the dimensions
+            dummy = np.loadtxt('../SpecTex/T{}/{}.txt'.format(n_img, k))
+            dummy = np.expand_dims(dummy, axis=2)
+            data = np.append(data, dummy, axis=2)
+    print('Finished loading data from image no.{}'.format(n_img))
+
+    # cycle on number of channels
+    for n in range(1, 9):
+        # generate the gaussian sensitivity functions
+        sensitivity = f_gauss(waves, n)
+        # # plot the functions
+        # for j in out:
+        #     plt.plot(j)
+        # plt.ylabel('gaussians')
+        # plt.show()
+
+        # generate empty simulated array, change third dimension so that it corresponds to number of filters
+        sim_shape = list(data.shape)
+        sim_shape[2] = n
+        sim_shape = tuple(sim_shape)
+        simulation = np.zeros(sim_shape)
+        # filter each pixel of data
+        for i in range(len(sensitivity)):
+            # weight each pixel of data on ith sensitivity and sum over third dimension
+            filtered = np.dot(data, sensitivity[i])
+            # generate directory, if needed
+            directory = '../SpecTex/T{}/Simulation{}'.format(n_img, n)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            # save filtered image, ith channel
+            np.savetxt('{}/{}.txt'.format(directory, i+1), filtered, delimiter=' ')
+            # export output to simulation variable
+            simulation[:, :, i] = filtered
+
+    # get how long does one iteration take
+    if n_img == '01':
+        print('One iteration takes approximately {}'.format(time.time() - time_start))
