b7c4f65cea54c73293216fefa8f117cd0934a569
helenebm <helene.midtfjord@outlook.com>
2018-03-26 14:26:39 +0200

    Removing index from output file

--- p_files/makePathsFile.py
+++ p_files/makePathsFile.py
@@ -8,21 +8,18 @@ import numpy as np
 import os 
 import os.path
 import pandas as pd
-import csv
 
 
 """ Settings """
-numberOfImages = 5
+numberOfImages = 4
 
-
-""" Make dataframe and excel file """
 
 length = np.zeros(shape = (numberOfImages,3))
 tabell = pd.DataFrame(data = length, columns = ['ID', 'Image', 'Mask']) # Create the dataframe
 directory = os.getcwd() # Set the current directory
 j = 0
-for i in range(0,numberOfImages+1):
-    maskName = '\mask.nrrd' #Making a name for the mask
+for i in range(1,numberOfImages+1):
+    maskName = '\mask.nrrd' # Making a name for the mask
     if i < 10:
         fileName = 'T0' + str(i) + '.nrrd' # Making a file name to check if the file is in the folder
         imageName = '\T0' + str(i) + '.nrrd'# Making a name for the new image file
@@ -30,10 +27,10 @@ for i in range(0,numberOfImages+1):
         fileName = 'T' + str(i) + '.nrrd'
         imageName = '\T' + str(i) + '.nrrd'
     if os.path.isfile(fileName):
-        tabell.iloc[j,0] = i        # A column for the image ID
+        tabell.iloc[j,0] = i       # A column for the image ID
         tabell.iloc[j,1] = directory +  imageName # A colummn for the path to the images
         tabell.iloc[j,2] = directory +  maskName  # A column for the path to the mask of the image
         j += 1  
     print(i)
-    
-tabell.to_csv('paths.csv') # Make a excel-file with the paths. 
+
+tabell.to_csv('paths.csv', index=None)
