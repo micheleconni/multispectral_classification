7683cc5adf886dc4b1e57530913611fa43b227dc
helenebm <helene.midtfjord@outlook.com>
2018-03-26 14:25:45 +0200

    mask into integer instead of float

--- p_files/makeMask.py
+++ p_files/makeMask.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 24 20:13:43 2018
-
-@author: helenebm
-"""
 import nrrd
 import numpy as np
 
@@ -18,13 +13,13 @@ y = imageShape[1]
 z = imageShape[2]
 
 # make mask
-mask = np.zeros((x,y,z))
+mask = np.zeros((x,y,z)).astype(int)
 for i in range(0,x):
     for j in range(0,y):
         for k in range(0,z):
-            mask[i][j][k]=1   
+            mask[i][j][k]=1  
 
 # write mask to nrrd file
-#nrrd.write(maskname, mask)
+nrrd.write(maskname, mask)
 
 print ('Mask shape', mask.shape)
