0a81dd8a53cbd1bd2d6c28e1b3ce2fed531269e8
helenebm <helene.midtfjord@outlook.com>
2018-03-29 12:07:00 +0200

    Update classifier_multiclass.py

--- p_files/classifier_multiclass.py
+++ p_files/classifier_multiclass.py
@@ -3,7 +3,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 
